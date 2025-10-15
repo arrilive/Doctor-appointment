d2ec39e7ea5ff19022d2210c15db7e796ebddb82
arrilive <lv20051309@gmail.com>
2025-10-15 16:35:20 -0600

    feat(layout): add breadcrum component and integrate into admin layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,9 @@
-<x-admin-layout>
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'href' => route('admin.dashboard')
+    ],
+    ['name' => 'Profile'],
+]">
     Hola desde admin
 </x-admin-layout>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,47 +1,55 @@
+@props(['breadcrumbs' => []])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Laravel') }}</title>
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+<!-- Scripts -->
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="https://kit.fontawesome.com/9161014f5f.js" crossorigin="anonymous"></script>
+
+<!-- Styles -->
+<head>
+    ...
+    <wireui:scripts />
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+@livewireStyles
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Vite Assets -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- WireUI -->
-    <wireui:scripts />
-
-    <!-- Styles -->
     @livewireStyles
-
-    <!-- FontAwesome -->
-    <script src="https://kit.fontawesome.com/ce759f6076.js" crossorigin="anonymous"></script>
 </head>
 <body class="font-sans antialiased bg-gray-50">
+@include('layouts.includes.admin.navigation')
+@include('layouts.includes.admin.sidebar')
 
-    <!-- Navbar -->
-    @include('layouts.includes.admin.navigation')
-
-    <!-- Sidebar -->
-    @include('layouts.includes.admin.sidebar')
-
-    <!-- Main Content -->
-    <main class="p-4 sm:ml-64 mt-14 bg-blue-200">
+<div class="p-4 sm:ml-64">
+    {{-- añadiendo margen superior --}}
+    <div class="mt-14 flex items-center justify-between w-full">
+        @include('layouts.includes.admin.breadcrumb')
+    </div>
     {{ $slot }}
-    </main>
+</div>
 
-    <!-- Modals -->
-    @stack('modals')
+@stack('modals')
 
-    <!-- Livewire Scripts -->
-    @livewireScripts
+@livewireScripts
+<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 
-    <!-- Flowbite -->
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 </html>
