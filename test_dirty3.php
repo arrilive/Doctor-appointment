0d4f562b2bb0f1f8d150eb7487e6da282c3a8e17
arrilive <lv20051309@gmail.com>
2026-03-17 16:32:05 -0600

    chore: remove debug files from repository

--- test_dirty3.php
+++
@@ -1,5 +0,0 @@
-<?php
-$user = App\Models\User::first();
-$user->address = $user->address ?? 'N/A';
-echo "is dirty: " . ($user->isDirty() ? 'yes' : 'no') . "\n";
-print_r($user->getDirty());
--- test_tinker.php
+++
@@ -1,9 +0,0 @@
-<?php
-$user = App\Models\User::first();
-$original = $user->getAttributes();
-$user->name = $original['name'];
-$user->email = $original['email'];
-$user->id_number = $original['id_number'];
-$user->phone = (string)$original['phone'];
-$user->address = $original['address'];
-echo json_encode($user->getDirty());
